4475e6f85fabeeef2aeb2d1e818e58d01ca47e38
Edson Filho <edson.er.rodrigues@gmail.com>
2025-05-13 17:05:13 -0300

    melhorias no layout das páginas

--- django/common/static/js/script.js
+++ django/common/static/js/script.js
@@ -1,4 +1,22 @@
-function toggleSidebar() {
-    const sidebar = document.querySelector('.sidebar');
-    sidebar.classList.toggle('collapsed');
+function togglePassword() {
+	const passwordField = document.querySelector('#id_password');
+	const toggleIcon = document.querySelector('#toggleIcon');
+
+	if (passwordField.type === 'password') {
+		passwordField.type = 'text';
+		toggleIcon.classList.replace('fa-eye-slash', 'fa-eye');
+	} else {
+		passwordField.type = 'password';
+		toggleIcon.classList.replace('fa-eye', 'fa-eye-slash');
+	}
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+	const passwordField = document.querySelector('#id_password');
+	const toggleIcon = document.querySelector('#toggleIcon');
+
+	if (passwordField && toggleIcon) {
+		passwordField.type = 'password';
+		toggleIcon.classList.add('fa-eye-slash');
+	}
+});
