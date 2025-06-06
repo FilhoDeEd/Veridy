d9a5b33b124e2b6a8efd9e790ba6a9a24876fff9
Edson Filho <edson.er.rodrigues@gmail.com>
2025-06-06 14:45:40 -0300

    modelo para documento

--- django/documents/models.py
+++ django/documents/models.py
@@ -1,3 +1,38 @@
-from django.db import models
+import hashlib
+import os
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+from django.utils.text import slugify
+
+from institution.models import Institution
+from subject.models import Subject
+
+
+def directory_path(instance, filename):
+    name, extension = os.path.splitext(filename)
+    safe_name = slugify(name)
+    institution_slug = slugify(instance.institution.name)
+
+    hash = hashlib.md5(f'{filename}{instance.subject.id}{instance.institution.id}'.encode()).hexdigest()
+
+    final_filename = f'{safe_name}_{hash[:8]}{extension}'
+    return f'documents/{institution_slug}/{final_filename}'
+
+
+class Document(models.Model):
+    file = models.FileField(upload_to=directory_path)
+    subject = models.ForeignKey(Subject, on_delete=models.PROTECT, related_name='documents')
+    institution = models.ForeignKey(Institution, on_delete=models.PROTECT, related_name='documents')
+
+    upload_date = models.DateTimeField(editable=False)
+    update_date = models.DateTimeField()
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.upload_date = timezone.now()
+        self.update_date = timezone.now()
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return os.path.basename(self.file.name)
