5f5571e8f82191ef8bc53d97430a56c156910018
Edson Filho <edson.er.rodrigues@gmail.com>
2025-06-09 16:07:29 -0300

    melhorias na barra de pesquisa

--- django/common/static/js/search.js
+++ django/common/static/js/search.js
@@ -3,14 +3,26 @@ const searchForm = document.getElementById('search-form');
 if (searchForm) {
 	const dropdownItems = searchForm.querySelectorAll('.dropdown-item');
 	const targetInput = searchForm.querySelector('#search-target');
-	const dropdownLabel = searchForm.querySelector('#search-dropdown span');
+	const dropdownIcon = searchForm.querySelector('#search-icon');
+
+	const savedTarget = localStorage.getItem('searchTarget');
+	const savedIcon = localStorage.getItem('searchIcon');
+	if (savedTarget && savedIcon) {
+		targetInput.value = savedTarget;
+		dropdownIcon.className = `fas ${savedIcon} theme-inverse`;
+	}
 
 	dropdownItems.forEach((item) => {
 		item.addEventListener('click', function (e) {
 			e.preventDefault();
 			const target = this.dataset.target;
+			const icon = this.dataset.icon;
+
 			targetInput.value = target;
-			dropdownLabel.innerText = this.innerText;
+			dropdownIcon.className = `fas ${icon} theme-inverse`;
+
+			localStorage.setItem('searchTarget', target);
+			localStorage.setItem('searchIcon', icon);
 		});
 	});
 
